4f237939977438e30e0098224dcc90997f1ef7c2
maheshpanda1806 <maheshpanda182003@gmail.com>
2024-08-03 13:45:39 +0530

    jj

---
+++ Trees/KthSmallestElementInBST.java
@@ -0,0 +1,40 @@
+public class KthSmallestElementInBST {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    //Inorder gives a sorted list then select the kth element (Hahahahahhahah) *laughing emoji*
+    int count;
+    int ans;
+   public int kthSmallest(TreeNode root, int k) {
+       count =0;
+       ans=0;
+       inorder(root,k);
+       return ans;
+   }
+   public void inorder(TreeNode root,int k){
+       if(root==null) return;
+       inorder(root.left,k);
+       //sout
+       count++;
+       if(count == k){
+         ans = root.val;
+         return;
+       }
+       inorder(root.right,k);
+   }
+ }
+}
--- Trees/PathSum.java
+++ Trees/PathSum.java
@@ -17,7 +17,8 @@ public class PathSum {
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
       if(root==null) return false;
-      if(targetSum-root.val==0 && root.left==null && root.right==null) return true;//root.left===null and 
+      if(targetSum-root.val==0 && root.left==null && root.right==null) return true;
+      //root.left===null and 
       //root.right==null issloye checkarna hai kyuki 
       //leaf node tak sum find karna hai(aisa question mein bola hai) aisa nhi kiye 
       //toh jidhar bhi zero hoga uske neeche check nhi karega
@@ -25,7 +26,6 @@ class Solution {
       if(left) return true;
       boolean right = hasPathSum(root.right,targetSum-root.val);
       if(right) return true;
-
       return false;
 
     }
